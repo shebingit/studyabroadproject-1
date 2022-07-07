a88d19d12506b9aa33a612bd8d05f7ac34da30b7
Your Name <you@example.com>
2022-07-07 21:54:00 +0530

    commit7/7

--- app1/urls.py
+++ app1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,re_path
 from . import views 
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('',views.login,name='login'),
     path('studens',views.studens, name='studens'),
@@ -11,6 +12,40 @@ urlpatterns = [
     path('previousleadss',views.previousleadss,name='previousleadss'),
     path('previousdash',views.previousdash,name='previousdash'),
     path('statuscurrent',views.statuscurrent,name='statuscurrent'),
+    path('IT_head_document_officers_list',views.IT_head_document_officers_list,name='IT_head_document_officers_list'),
+    path('IT_head_DocumentOfficer1',views.IT_head_DocumentOfficer1,name='IT_head_DocumentOfficer1'),
+    path('IT_head_DocumentOfficer2',views.IT_head_DocumentOfficer2,name='IT_head_DocumentOfficer2'),
+    path('IT_head_DocumentOfficer3',views.IT_head_DocumentOfficer3,name='IT_head_DocumentOfficer3'),
+    path('IT_head_DocumentOfficer_newly_assign_leads',views.IT_head_DocumentOfficer_newly_assign_leads,name='IT_head_DocumentOfficer_newly_assign_leads'),
+    path('IT_head_DocumentOfficer_current_leads',views.IT_head_DocumentOfficer_current_leads,name='IT_head_DocumentOfficer_current_leads'),
+    path('IT_head_DocumentOfficer_previous_leads',views.IT_head_DocumentOfficer_previous_leads,name='IT_head_DocumentOfficer_previous_leads'),
+     path('IT_head_DocumentOfficer_current_canadaDocuments',views.IT_head_DocumentOfficer_current_canadaDocuments,name='IT_head_DocumentOfficer_current_canadaDocuments'),
+    path('IT_head_Teamlead_currentlead',views.IT_head_Teamlead_currentlead,name='IT_head_Teamlead_currentlead'),
+    path('IT_head_Teamlead_previoslead',views.IT_head_Teamlead_previoslead,name='IT_head_Teamlead_previoslead'),
+    
+    path('IT_head_DocumentOfficer_current_UKDocuments',views.IT_head_DocumentOfficer_current_UKDocuments,name='IT_head_DocumentOfficer_current_UKDocuments'),
+    
+    path('IT_head_DocumentOfficer_previous_UKDocuments',views.IT_head_DocumentOfficer_previous_UKDocuments,name='IT_head_DocumentOfficer_previous_UKDocuments'),
+    
+    path('IT_head_DocumentOfficer_previous_canadaDocuments',views.IT_head_DocumentOfficer_previous_canadaDocuments,name='IT_head_DocumentOfficer_previous_canadaDocuments'),
+    
+    
+    path('IT_head_add_lead',views.IT_head_add_lead,name='IT_head_add_lead'),
+    
+    
+    
+    path('Team_Lead_dashboard',views.Team_Lead_dashboard,name='Team_Lead_dashboard'),
+    path('Team_Lead_current_leads',views.Team_Lead_current_leads,name='Team_Lead_current_leads'),
+    path('Team_Lead_previous_leads',views.Team_Lead_previous_leads,name='Team_Lead_previous_leads'),
+    path('Team_Lead_new_leads',views.Team_Lead_new_leads,name='Team_Lead_new_leads'),
+    path('Team_Lead_current_canadaDocuments',views.Team_Lead_current_canadaDocuments,name='Team_Lead_current_canadaDocuments'),
+    path('Team_Lead_current_UKDocuments',views.Team_Lead_current_UKDocuments,name='Team_Lead_current_UKDocuments'),
+    path('Team_Lead_previous_canadaDocuments',views.Team_Lead_previous_canadaDocuments,name='Team_Lead_previous_canadaDocuments'),
+    path('Team_Lead_previous_UKDocuments',views.Team_Lead_previous_UKDocuments,name='Team_Lead_previous_UKDocuments'),
+    
+    
+    
+    
     #dilashad
     path('dashboard',views.dashboard, name='dashboard' ),
     path('team_leader_clints',views.team_leader_clints,name='team_leader_clints'),
@@ -40,4 +75,44 @@ urlpatterns = [
     re_path(r'^DocumentOfficer_Previous_CanadaDocuments$', views.DocumentOfficer_Previous_CanadaDocuments, name='DocumentOfficer_Previous_CanadaDocuments'),
     re_path(r'^DocumentOfficer_Previous_UKDocuments$', views.DocumentOfficer_Previous_UKDocuments, name='DocumentOfficer_Previous_UKDocuments'),
     
+    
+     #shebin shaji Admin section
+    path('load_admin',views.load_admin,name='load_admin'),
+    path('load_leads',views.load_leads,name='load_leads'),
+    path('load_concilors',views.load_concilors,name='load_concilors'),
+    path('load_docofficers',views.load_docofficers,name='load_docofficers'),
+    path('load_documets',views.load_documets,name='load_documets'),
+    path('current_lead',views.current_lead,name='current_lead'),
+    path('load_completed',views.load_completed,name='load_completed'),
+    path('load_cuttentdoc',views.load_cuttentdoc,name='load_cuttentdoc'),
+    path('load_priviousdoc',views.load_priviousdoc,name='load_priviousdoc'),
+    path('load_admin_profile',views.load_admin_profile,name='load_admin_profile'),
+    path('load_previous_lead',views.load_previous_lead,name='load_previous_lead'),
+    path('load_previous_consilers',views.load_previous_consilers,name='load_previous_consilers'),
+    path('load_admin_clients_more',views.load_admin_clients_more,name='load_admin_clients_more'),
+    path('load_admin_client_compstatus',views.load_admin_client_compstatus,name='load_admin_client_compstatus'),
+    path('load_admin_client_currentstatus',views.load_admin_client_currentstatus,name='load_admin_client_currentstatus'),
+
+    #yamuna
+    path('load_hr_dashboard',views.load_hr_dashboard,name='load_hr_dashboard'),
+    path('attendance',views.attendance,name='attendance'),
+    path('attendancecard',views.attendancecard,name='attendancecard'),
+    path('addteamlead',views.addteamlead,name='addteamlead'),
+    path('load_studcounslerprofile',views.load_studcounslerprofile,name='load_studcounslerprofile'),
+    path('currentstucounsiler',views.currentstucounsiler,name='currentstucounsiler'),
+    path('ithead',views.ithead, name='ithead'),
+    path('currentithead',views.currentithead,name='currentithead'),
+    path('previousithead',views.previousithead,name='previousithead'),
+    path('documentofficerprofile',views.documentofficerprofile,name='documentofficerprofile'),
+    path('documentofficers',views.documentofficers,name='documentofficers'),
+    path('documentofficercard',views.documentofficercard,name='documentofficercard'),
+    path('currentdocoff',views.currentdocoff,name='currentdocoff'),
+    path('previousdocoff',views.previousdocoff,name='previousdocoff'),
+    path('previousdocoffprofile',views.previousdocoffprofile,name='previousdocoffprofile'),
+    path('currentdocoffprofile',views.currentdocoffprofile,name='currentdocoffprofile'),
+    path('attendanceview',views.attendanceview,name='attendanceview'),
+    path('attendancesearch',views.attendancesearch,name='attendancesearch'),
+    path('studentcouncilorcard',views.studentcouncilorcard,name='studentcouncilorcard'),
+    path('previousstudcounc',views.previousstudcounc,name='previousstudcounc'),
+    path('teamprofile',views.teamprofile,name='teamprofile'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 #sherry george
 def login(request):
     return render(request,'login.html')
@@ -17,7 +17,62 @@ def previousleadss(request):
 def previousdash(request):
     return render(request,'previousleads.html')     
 def statuscurrent(request):
-    return render(request,'status1.html')      
+    return render(request,'status1.html')   
+
+    #IT HEAD   
+
+def IT_head_document_officers_list(request):
+    return render(request,'IT_head_document_officers_list.html')      
+def IT_head_DocumentOfficer1(request):
+    return render(request,'IT_head_DocumentOfficer1.html')      
+def IT_head_DocumentOfficer2(request):
+    return render(request,'IT_head_DocumentOfficer2.html')      
+def IT_head_DocumentOfficer3(request):
+    return render(request,'IT_head_DocumentOfficer3.html')      
+def IT_head_DocumentOfficer_newly_assign_leads(request):
+    return render(request,'IT_head_DocumentOfficer_newly_assign_leads.html')      
+def IT_head_DocumentOfficer_current_leads(request):
+    return render(request,'IT_head_DocumentOfficer_current_leads.html')      
+def IT_head_DocumentOfficer_previous_leads(request):
+    return render(request,'IT_head_DocumentOfficer_previous_leads.html') 
+def IT_head_Teamlead_currentlead(request):
+    return render(request,'IT_head_Teamlead_currentlead.html')
+def IT_head_Teamlead_previoslead(request):
+    return render(request,'IT_head_Teamlead_previouslead.html')
+
+def IT_head_add_lead(request):
+    return render(request,'IT_head_add_lead.html') 
+
+
+
+def IT_head_DocumentOfficer_current_canadaDocuments(request):
+    return render(request,'IT_head_DocumentOfficer_current_canadaDocuments.html') 
+def IT_head_DocumentOfficer_current_UKDocuments(request):
+    return render(request,'IT_head_DocumentOfficer_current_UKDocuments.html') 
+def IT_head_DocumentOfficer_previous_UKDocuments(request):
+    return render(request,'IT_head_DocumentOfficer_previous_UKDocuments.html') 
+def IT_head_DocumentOfficer_previous_canadaDocuments(request):
+    return render(request,'IT_head_DocumentOfficer_previous_canadaDocuments.html')
+
+
+
+def Team_Lead_dashboard(request):
+    return render(request,'Team_Lead_dashboard.html') 
+def Team_Lead_current_leads(request):
+    return render(request,'Team_Lead_current_leads.html') 
+def Team_Lead_previous_leads(request):
+    return render(request,'Team_Lead_previous_leads.html') 
+def Team_Lead_new_leads(request):
+    return render(request,'Team_Lead_new_leads.html') 
+
+def Team_Lead_current_canadaDocuments(request):
+    return render(request,'Team_Lead_current_canadaDocuments.html')
+def Team_Lead_current_UKDocuments(request):
+    return render(request,'Team_Lead_current_UKDocuments.html')
+def Team_Lead_previous_canadaDocuments(request):
+    return render(request,'Team_Lead_previous_canadaDocuments.html')
+def Team_Lead_previous_UKDocuments(request):
+    return render(request,'Team_Lead_previous_UKDocuments.html')    
 
 #dilshad
 def dashboard(request):
@@ -84,3 +139,97 @@ def DocumentOfficer_Previous_CanadaDocuments(request):
 def DocumentOfficer_Previous_UKDocuments(request):
     return render(request,"DocumentOfficer_Previous_UKDocuments.html")    
 
+
+
+
+#Shebin Shaji ==> admin
+
+def load_admin(request):
+    return render(request,'admin.html')
+
+def load_leads(request):
+    return render(request,'admin_leads.html')
+
+def load_concilors(request):
+    return render(request,'admin_concilors.html')
+
+def load_docofficers(request):
+    return render(request,'admin_docofficers.html')
+
+def load_documets(request):
+    return render(request,'admin_document.html')
+
+def load_admin_profile(request):
+    return render(request,'admin_profile.html')
+
+def load_cuttentdoc(request):
+    return render(request,'admin_currentdoc.html')
+
+def load_priviousdoc(request):
+    return render(request,'admin_priviousdoc.html')
+
+def load_completed(request):
+    return render(request,'admin_completed.html')
+
+def load_previous_lead(request):
+    return render(request,'admin_previs_leads.html')
+
+def current_lead(request):
+    return render(request,'admin_current_leads.html')
+
+def load_previous_consilers(request):
+    return render(request,'admin_prev_consilers.html')
+
+def load_admin_clients_more(request):
+    return render(request,'admin_client_more.html')
+
+def load_admin_client_compstatus(request):
+    return render(request,'admin_client_status_comp.html')
+
+def load_admin_client_currentstatus(request):
+    return render(request,'admin_client_status_current.html')
+
+
+#Yamuna 
+def load_hr_dashboard(request):
+    return render(request,'dashboard_hr.html')
+def attendance(request):
+    return render(request,'attendance.html')
+def addteamlead(request):
+    return render(request,'addteamlead.html')
+def ithead(request):
+    return render(request,'itheadprofile.html')
+def currentithead(request):
+    return render(request,'currentithead.html')
+def previousithead(request):
+    return render(request,'previousithead.html')
+def documentofficerprofile(request):
+    return render(request,'documentofficerprofile.html')
+def documentofficers(request):
+    return render(request,'documentofficers.html')
+def documentofficercard(request):
+    return render(request,'documentofficercard.html')
+def currentdocoff(request):
+    return render(request,'currentdocoff.html')
+def previousdocoff(request):
+    return render(request,'previousdocoff.html')
+def attendancecard(request):
+    return render(request,'attendancecard.html')
+def attendanceview(request):
+    return render(request,'attendanceview.html')
+def attendancesearch(request):
+    return render(request,'attendancesearch.html')
+def studentcouncilorcard(request):
+    return render(request,'studentcouncilorcard.html')
+def previousstudcounc(request):
+    return render(request,'previousstudcounc.html')
+def teamprofile(request):
+    return render(request,'teamprofile.html')
+def previousdocoffprofile(request):
+    return render(request,'previousdocoffprofile.html')
+def currentstucounsiler(request):
+    return render(request,'stud_currentcounsiler.html')
+def load_studcounslerprofile(request):
+    return render(request,'studcounsilerprofile.html')
+def currentdocoffprofile(request):
+    return render(request,'currentdocoffprofile.html')
